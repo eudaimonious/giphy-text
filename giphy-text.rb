4fdf3e771a67ce2494764d04fc305d7b72b80ec7
Margaret Scott <margaretscott@patientslikeme.com>
2016-06-07 17:04:34 -0400

    submit a url instead of an array of giphy objects

--- giphy-text.rb
+++ giphy-text.rb
@@ -11,13 +11,16 @@ post '/form' do
   rest_client.account.messages.create(
     from: ENV['TWILIO_NUMBER'],
     to: ENV['AMY'],
-    media_url: Giphy.translate(params[:text])
+    media_url: gif_url(params[:text])
   )
 end
 
 def rest_client
   account_sid = ENV['TWILIO_ACCOUNT_SID']
   auth_token = ENV['TWILIO_AUTH_TOKEN']
- 
   client = Twilio::REST::Client.new account_sid, auth_token
+end
+
+def gif_url(text)
+ Giphy.translate(text).first.url
 end
